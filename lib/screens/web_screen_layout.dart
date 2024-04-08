d34613c368b21ec8d5eb5b222148953d6723b786
MahendraPatel08 <mrmahendrapatel08gmail.com>
2024-04-08 17:53:30 +0530

    adding web chat inputField

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/widgets/chat_list.dart';
 import 'package:whatsapp_clone/widgets/contact_list.dart';
 import 'package:whatsapp_clone/widgets/web_chat_appbar.dart';
@@ -32,10 +33,70 @@ class WebScreenLayout extends StatelessWidget {
                   image: AssetImage('assets/backgroundimage.png'),
                   fit: BoxFit.cover),
             ),
-            child: const Column(
+            child: Column(
               children: [
-                WebChatAppbar(),
-                Expanded(child: ChatList()),
+                const WebChatAppbar(),
+                const Expanded(
+                  child: ChatList(),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: dividerColor),
+                    ),
+                    color: chatBarMessage,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.emoji_emotions_outlined,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.attach_file,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            right: 15,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: searchBarColor,
+                              hintText: 'Type a message',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                                borderSide: const BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.only(left: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.mic,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           )
