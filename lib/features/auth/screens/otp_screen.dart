c57133e73eec5b9a1e106099878cd1e113666fa2
MahendraPatel08 <mrmahendrapatel08gmail.com>
2024-04-09 17:47:27 +0530

    add otp screen and build phone auth logic

---
+++ lib/features/auth/screens/otp_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OTPScreen extends StatefulWidget {
+  static const String routName = '/otp-screen';
+  const OTPScreen({super.key, required this.verificationId});
+
+  final String verificationId;
+
+  @override
+  State<OTPScreen> createState() => _OTPScreenState();
+}
+
+class _OTPScreenState extends State<OTPScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/common/widgets/error.dart';
 import 'package:whatsapp_clone/features/auth/screens/login_screen.dart';
+import 'package:whatsapp_clone/features/auth/screens/otp_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case LoginScreen.routeName:
       return MaterialPageRoute(
         builder: (context) => const LoginScreen(),
+      );
+    case OTPScreen.routName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (context) => OTPScreen(
+          verificationId: verificationId,
+        ),
       );
     default:
       return MaterialPageRoute(
